e886a5f0d7db92f9778d3374510bf34833b9ca16
Aidan Mora <aidanmora2004@gmail.com>
2025-10-27 19:28:05 -0400

    - Added the setter for gold earned
       - I added it so that the AdventurerStreams.java bonusGoldEvent stream processing method can set the new gold amount.

--- src/Adventurer.java
+++ src/Adventurer.java
@@ -6,7 +6,7 @@ public class Adventurer implements Comparable<Adventurer> {
     private final String name;
     private final int age;
     private final String role;
-    private final Double goldEarned;
+    private Double goldEarned;
     private final List<Skill> Skills;
 
     public Adventurer(String name, int age, String role, Double goldEarned, List<Skill> skills) {
@@ -28,6 +28,9 @@ public class Adventurer implements Comparable<Adventurer> {
     }
     public Double getGoldEarned() {
         return goldEarned;
+    }
+    public void setGoldEarned(Double gold) {
+        this.goldEarned = gold;
     }
     public List<Skill> getSkills() {return new ArrayList<>(Skills);}
     // will create a list of skills that are in the original list, so that this list can be
@@ -54,18 +57,7 @@ public class Adventurer implements Comparable<Adventurer> {
         // if its like testName, 21 compare -> testName2, 50
             // then its negative since testName is younger than testName2
     }
-    //Compares by Role (or by name if role is the same)
-    public static final Comparator<Adventurer> BY_ROLE_NAME =
-            Comparator.comparing(Adventurer::getRole, String.CASE_INSENSITIVE_ORDER)
-                    .thenComparing(Adventurer::getName, String.CASE_INSENSITIVE_ORDER);
-    //using a lambda! Would be: a -> a.getRole() |
-        // Adventurer class using the getRole getter,
-        //compares the adventurer on role, BUT it compares that string role without paying attention
-        // to the case (could be fletcher or Fletcher [still compares] )
-    // The second role here is if the adventurer has the same role, we compare those by name
-        // (ignoring case still)
 
-    public static final
 
     public String toString() {
         return "This is a toString: " + name + age + role + goldEarned;
