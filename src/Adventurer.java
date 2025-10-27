276d637a9989e6287c74e6dfbd52e67f004d5c79
Aidan Mora <aidanmora2004@gmail.com>
2025-10-27 16:50:42 -0400

    - Added Comparator (BY_ROLE_NAME)
      - //using a lambda! Would be: a -> a.getRole() |
          Adventurer class using the getRole getter,compares the adventurer on role, BUT it compares that string role without paying attention to the case (could be fletcher or Fletcher [still compares] ) The second role here is if the adventurer has the same role, we compare those by name (ignoring case still)

--- src/Adventurer.java
+++ src/Adventurer.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Adventurer implements Comparable<Adventurer> {
@@ -40,6 +41,9 @@ public class Adventurer implements Comparable<Adventurer> {
         return Skills.size();
     }
 
+    // - Comparator Adventurer using streams -
+
+
     @Override
     public int compareTo(Adventurer o) {
         int byName = String.CASE_INSENSITIVE_ORDER.compare(this.name, o.name);
@@ -50,7 +54,18 @@ public class Adventurer implements Comparable<Adventurer> {
         // if its like testName, 21 compare -> testName2, 50
             // then its negative since testName is younger than testName2
     }
+    //Compares by Role (or by name if role is the same)
+    public static final Comparator<Adventurer> BY_ROLE_NAME =
+            Comparator.comparing(Adventurer::getRole, String.CASE_INSENSITIVE_ORDER)
+                    .thenComparing(Adventurer::getName, String.CASE_INSENSITIVE_ORDER);
+    //using a lambda! Would be: a -> a.getRole() |
+        // Adventurer class using the getRole getter,
+        //compares the adventurer on role, BUT it compares that string role without paying attention
+        // to the case (could be fletcher or Fletcher [still compares] )
+    // The second role here is if the adventurer has the same role, we compare those by name
+        // (ignoring case still)
 
+    public static final
 
     public String toString() {
         return "This is a toString: " + name + age + role + goldEarned;
