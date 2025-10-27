c25257df1b6dbb6697a1453d8189abe416ea834f
Aidan Mora <aidanmora2004@gmail.com>
2025-10-27 19:59:33 -0400

    - Added the getter for the adventurer list

--- src/Guild.java
+++ src/Guild.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**@Author Aidan Mora
  * Date: 10/22/2025
@@ -6,8 +8,8 @@ import java.util.ArrayList;
  */
 
 public class Guild {
-    String name;
-    ArrayList<Adventurer> adventurers;
+    private String name;
+    private List<Adventurer> adventurers;
 
     public Guild(String name, ArrayList<Adventurer> adventurers) {
         this.name = name;
@@ -24,5 +26,9 @@ public class Guild {
 
     public String toString(){
         return "This is a toString: " + name;
+    }
+
+    public List<Adventurer> getAdventurers() {
+        return adventurers;
     }
 }
