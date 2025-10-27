74e189a7b8a122403cfad640b60bfecd4d770e18
Aidan Mora <aidanmora2004@gmail.com>
2025-10-27 16:08:27 -0400

    - Added Getters:     - getSkills     - getSkillCount     - hasSkill - getSkill / hasSkill - getSkill will just return a new array list that contains the same skill elements as the actual skills list. So it returns the sames elements just not the same object inside memory. Just so we don't end up changing the list, and we end up changing the original list. Makes everything safer.
    - hasSkill is just checking the skills list to see if it contains any skills multiple or one.

--- src/Adventurer.java
+++ src/Adventurer.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
+import java.util.List;
 
-public class Adventurer {
-    String name;
-    int age;
-    String role;
-    Double goldEarned;
-    ArrayList<Adventurer> adventurers;
+public class Adventurer implements Comparable<Adventurer> {
+    private final String name;
+    private final int age;
+    private final String role;
+    private final Double goldEarned;
+    private final List<Skill> Skills;
 
-    public Adventurer(String name, int age, String role, Double goldEarned,  ArrayList<Adventurer> adventurers) {
+    public Adventurer(String name, int age, String role, Double goldEarned, List<Skill> skills) {
         this.name = name;
         this.age = age;
         this.role = role;
         this.goldEarned = goldEarned;
-        this.adventurers = adventurers;
+        this.Skills = skills = new ArrayList<>(skills);
     }
 
     public String getName() {
@@ -27,8 +28,21 @@ public class Adventurer {
     public Double getGoldEarned() {
         return goldEarned;
     }
+    public List<Skill> getSkills() {return new ArrayList<>(Skills);}
 
+    public boolean hasSkill(Skill s){ //helper method for the stream filters
+        return Skills.contains(s);
+    }
+
+    public int getSkillCount(){ // another helper method for stream filters I'm going to make later
+        return Skills.size();
+    }
     public String toString() {
         return "This is a toString: " + name + age + role + goldEarned;
+    }
+
+    @Override
+    public int compareTo(Adventurer o) {
+        return 0;
     }
 }
